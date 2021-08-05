fc704cd33579b79deec7ac449a2aba493e90de23
Sharki <Sharkismanagements@gmail.com>
2021-08-05 15:50:41 +0200

    Steps 1.8, 1.9 and 1.10

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,24 +1,47 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const Title = (props) => <h1>{props.text}</h1>
+const Title = (props) => (<h1>{props.text}</h1>)
 
-const Button = ({ onClick, text }) => {
+const Statistics = ({ good, neutral, bad }) => {
+  const total = good + neutral + bad
+  const positive = good === 0 ? `${0} %` : `${(good * 100) / total} %`
+  const average = good + bad * -1
+
+  if (good !== 0 || neutral !== 0 || bad !== 0) {
+    return (
+      <div>
+        <Statistic text="Good" value={good} />
+        <Statistic text="Neutral" value={neutral} />
+        <Statistic text="Bad" value={bad} />
+        <Statistic text="Total" value={total} />
+        <Statistic text="Average" value={average} />
+        <Statistic text="Positive" value={positive} />
+      </div>
+    )
+  }
+
+  return <p>No feedback given</p>
+}
+
+const Statistic = ({ text, value }) => (<p> {text}: {value}</p>)
+
+const Buttons = ({ clickHandleGood, clickHandleNeutral, clickHandleBad }) => {
   return (
-    <button onClick={onClick}>{text}</button>
+    <>
+      <Button onClick={clickHandleGood} text="Good" />
+      <Button onClick={clickHandleNeutral} text="Neutral" />
+      <Button onClick={clickHandleBad} text="Bad" />
+    </>
   )
 }
 
-const Text = ({ text, points }) => <p>{text} {points}</p>
+const Button = ({ onClick, text }) => (<button onClick={onClick}>{text}</button>)
 
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  
-  const total = good + neutral + bad
-  const positive = good == 0 ? `${0} %` : `${(good * 100) / total} %`
-  const average = good + bad * -1
 
   const clickHandleGood = () => setGood(good + 1)
   const clickHandleNeutral = () => setNeutral(neutral + 1)
@@ -27,16 +50,13 @@ const App = () => {
   return (
     <div>
       <Title text='give feedback' />
-      <Button onClick={clickHandleGood} text="Good" />
-      <Button onClick={clickHandleNeutral} text="Neutral" />
-      <Button onClick={clickHandleBad} text="Bad" />
+      <Buttons
+        clickHandleGood={clickHandleGood}
+        clickHandleNeutral={clickHandleNeutral}
+        clickHandleBad={clickHandleBad}
+      />
       <Title text='statistics' />
-      <Text text="good" points={good} />
-      <Text text="neutral" points={neutral} />
-      <Text text="bad" points={bad} />
-      <Text text="all" points={total} />
-      <Text text="positives" points={positive} />
-      <Text text="average" points={average} />
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
